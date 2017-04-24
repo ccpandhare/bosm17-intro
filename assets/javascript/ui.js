5eb311c429db376a742f84f34a2d7dd0e4fef09b
Roboneet <neethumariyajoy@gmail.com>
2017-04-24 23:09:06 +0530

    small glich fixes

--- assets/javascript/ui.js
+++ assets/javascript/ui.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
-	$(".backdrop").fadeOut(100);
+
+
+
+	$('#two .text').addClass('appear-animation');
+	$('#two .box').addClass('frame-animation');
 
 
 
 	$("#two footer .button").click(function(){
+		$(".backdrop").css({"display": "flex"});
 		$(".backdrop").fadeIn();
 
 		curr_id = $(this).attr("list");
-		player.loadVideoById({videoId:youtube_video_ids[curr_id],startSeconds:40,suggestedQuality:'large'});
+		
+		player.loadVideoById({videoId:youtube_video_ids[curr_id],startSeconds:10,suggestedQuality:'large'});
+
 
 	});
 
@@ -19,37 +26,37 @@ $(document).ready(function(){
       	$(".backdrop").fadeOut();
 	});
 
-	function adjust_footer_height(){
-		var footer_height_not = $("#two header").height() + $("#two section").height();
-		var window_height = $(window).height();
-		console.log("called");
-		if($(window).width() <900){
-			if(window_height - footer_height_not > 144){
-				$("#two footer").css({'height': window_height - footer_height_not + "px" ,
-									'position': 'absolute',
-									'bottom' : '0',
-									});
-			}else{
-				$("#two footer").css({'height': "auto" ,
-									'position': 'relative',
-									});
-			}
+	// function adjust_footer_height(){
+	// 	var footer_height_not = $("#two header").height() + $("#two section").height();
+	// 	var window_height = $(window).height();
+	// 	console.log("called");
+	// 	if($(window).width() <900){
+	// 		if(window_height - footer_height_not > 144){
+	// 			$("#two footer").css({'height': window_height - footer_height_not + "px" ,
+	// 								'position': 'absolute',
+	// 								'bottom' : '0',
+	// 								});
+	// 		}else{
+	// 			$("#two footer").css({'height': "auto" ,
+	// 								'position': 'relative',
+	// 								});
+	// 		}
 
-		}else{
-			$("#two footer").css({'height':'20vh'})
-		}
+	// 	}else{
+	// 		$("#two footer").css({'height':'20vh'})
+	// 	}
 
-	}
+	// }
 
-	adjust_footer_height();
-	$(window).resize(adjust_footer_height);
+	// adjust_footer_height();
+	// $(window).resize(adjust_footer_height);
 
 
-	$('#two header label').click(function(){
-		console.log("called here")
-		var k = setTimeout(adjust_footer_height,100);
-		var n = setTimeout(adjust_footer_height,1000);
-	 });
+	// $('#two header label').click(function(){
+	// 	console.log("called here")
+	// 	var k = setTimeout(adjust_footer_height,100);
+	// 	var n = setTimeout(adjust_footer_height,1000);
+	//  });
 
 
 
