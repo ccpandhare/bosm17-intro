b84a34ed5869d55395ff82f60f0f3dd568ae741f
Chinmay Pandhare <pandharechinmay882@gmail.com>
2017-04-26 17:02:36 +0530

    Commit 3

--- timer/js/timer.js
+++ timer/js/timer.js
@@ -1,4 +1,4 @@
-var DRUM_TEXTURE = "http://keithclark.co.uk/labs/css-fps/drum2.png";
+var DRUM_TEXTURE = "";
 
 // Assembiles are for grouping faces and other assembiles
 function createAssembly() {
@@ -41,29 +41,34 @@ function createBarrel() {
     var minht = 350;
     var ht = 0.8 * Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     if (ht < minht) ht = minht;
-    var barrel = createTube(ht, 100, 20, DRUM_TEXTURE);
+    var barrel = createTube(ht, 150, 30, DRUM_TEXTURE);
     document.barrel = barrel;
     return barrel;
 }
 
 function initTimer() {
 document.querySelector('.assembly').style.width = 1.25*parseInt(document.querySelector('.assembly .face').style.width)+"px";
-var times = ["3 Months","1 Month","20 Days","10 Days","5 Days","4 Days","3 Days","2 Days","1 Day","0 Days"];
+var times = ["3 Months","2 Months","1 Month","25 Days","20 Days","15 Days","2 Weeks","10 Days","1 Week","5 Days","4 Days","3 Days","2 Days","1 Day","0 Days"];
 var labels = document.querySelectorAll("#timer label");
 var today = new Date();
 var bosm = new Date("08/27/2017");
 var timeDiff = Math.abs(bosm.getTime() - today.getTime());
 var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
 if (diffDays >= 90) {var initial = 0;}
-else if (diffDays >= 30) {var initial = 1;}
-else if (diffDays >= 20) {var initial = 2;}
-else if (diffDays >= 10) {var initial = 3;}
-else if (diffDays >= 5) {var initial = 4;}
-else if (diffDays == 4) {var initial = 5;}
-else if (diffDays == 3) {var initial = 6;}
-else if (diffDays == 2) {var initial = 7;}
-else if (diffDays == 1) {var initial = 8;}
-else {var initial = 9;}
+else if (diffDays >= 60) {var initial = 1;}
+else if (diffDays >= 30) {var initial = 2;}
+else if (diffDays >= 25) {var initial = 3;}
+else if (diffDays >= 20) {var initial = 4;}
+else if (diffDays >= 15) {var initial = 5;}
+else if (diffDays >= 14) {var initial = 6;}
+else if (diffDays >= 10) {var initial = 7;}
+else if (diffDays >= 7) {var initial = 8;}
+else if (diffDays >= 5) {var initial = 9;}
+else if (diffDays == 4) {var initial = 10;}
+else if (diffDays == 3) {var initial = 11;}
+else if (diffDays == 2) {var initial = 12;}
+else if (diffDays == 1) {var initial = 13;}
+else {var initial = 14;}
 var j = 0;
 for (var i = initial; i < labels.length; i++) {
   labels[i-initial].innerHTML = times[i];
@@ -74,20 +79,20 @@ for (i = 0; j < labels.length; j++, i++) {
 }
 
 var faces = document.querySelectorAll('.face');
-var ct1 = faces[1].style.transform;
-var nt1 = ct1.replace(/Y\([^\)]*rad\)/,"Y(0.93rad)");
-var nt1 = nt1.replace(/X\([^\)]*rad\)/,"X(-0.02rad)");
-var ct19 = faces[19].style.transform;
-var nt19 = ct19.replace(/Y\([^\)]*rad\)/,"Y(-0.93rad)");
-var nt19 = nt19.replace(/X\([^\)]*rad\)/,"X(-0.02rad)");
-nt1 += " scale(3.5)";
-nt19 += " scale(3.5)";
-faces[1].style.transform = nt1;
+// var ct1 = faces[1].style.transform;
+// var nt1 = ct1.replace(/Y\([^\)]*rad\)/,"Y(0.93rad)");
+// var nt1 = nt1.replace(/X\([^\)]*rad\)/,"X(-0.02rad)");
+// var ct19 = faces[19].style.transform;
+// var nt19 = ct19.replace(/Y\([^\)]*rad\)/,"Y(-0.93rad)");
+// var nt19 = nt19.replace(/X\([^\)]*rad\)/,"X(-0.02rad)");
+// nt1 += " scale(3.5)";
+// nt19 += " scale(3.5)";
+// faces[1].style.transform = nt1;
 // faces[1].querySelector('span').style.transform = "rotateZ(-90deg) rotateX(-45deg) scaleY(0.75)";
-faces[1].style.transformOrigin = "left";
+// faces[1].style.transformOrigin = "left";
 // faces[19].querySelector('span').style.transform = "rotateZ(-90deg) rotateX(45deg) scaleY(0.75)";
-faces[19].style.transformOrigin = "right";
-faces[19].style.transform = nt19;
+// faces[19].style.transformOrigin = "right";
+// faces[19].style.transform = nt19;
 }
 
 
